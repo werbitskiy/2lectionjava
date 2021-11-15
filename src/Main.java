feb6b7df47321348f2f3f3760c2bf61df9729ca5
werbitskiy <kirverbitskiy@gmail.com>
2021-11-15 21:44:13 +0700

    Новый код 2 задания

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 600;
-        int minpayment = 500;
-        int payment = 2589;
+        int minpayment = 1000;
+        int payment = 1001;
 
         // 1 бонусный рубль за каждые 100р пополнения
         // минимальный платеж 500р
 
-        int bonusaccrual = payment/100;
-
-        if (payment<minpayment){
+        int bonusaccrual = 0;
+        if (payment<minpayment) {
             bonusaccrual = 0;
+        } else {
+            bonusaccrual = payment/100;
         }
 
-        int totalbalance = balance + payment ;
-        System.out.println("Ваш баланс: "+totalbalance+" рублей Начисленные бонусы: "+bonusaccrual+" рублей");
+
+        int totalbalance = balance + payment + bonusaccrual;
+        System.out.println("Ваш баланс: "+totalbalance+" рублей");
 
 
     }
